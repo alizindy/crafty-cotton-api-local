5392cba4ebf3778fd57897962625ce49e2a68096
Patipol Wangjaitham <patipol@shopdit.com>
2024-10-08 02:37:16 +0700

    chore: :wrench: Add docker and config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configService } from './config/config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      ...configService.getTypeOrmConfig(),
+      autoLoadEntities: true,
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
