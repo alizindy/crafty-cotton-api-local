e96a5e98fa2f47aa2c6b181575b989f01a655cab
Patipol Wangjaitham <patipol@shopdit.com>
2024-10-18 02:11:01 +0700

    docs: :memo: Add api operation for staff

--- src/staffs/staffs.controller.ts
+++ src/staffs/staffs.controller.ts
@@ -12,26 +12,29 @@ import {
 } from '@nestjs/common';
 import {
   ApiTags,
+  ApiOperation,
   ApiCreatedResponse,
   ApiResponse,
   ApiBody,
+  ApiBearerAuth,
 } from '@nestjs/swagger';
 import { StaffsService } from './staffs.service';
 import { CreateStaffDto } from './dto/create-staff.dto';
 import { UpdateStaffDto } from './dto/update-staff.dto';
-import { Staff } from './entities/staff.entity';
 
 @ApiTags('Staff')
+@ApiBearerAuth('JWT')
 @Controller('staffs')
 export class StaffsController {
   constructor(private readonly staffsService: StaffsService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a new staff member' })
   @ApiCreatedResponse({
     description: 'Creates a new staff member and associated user',
   })
   @ApiBody({ type: CreateStaffDto })
-  async create(@Body() createStaffDto: CreateStaffDto): Promise<Staff> {
+  async create(@Body() createStaffDto: CreateStaffDto) {
     try {
       return await this.staffsService.create(createStaffDto);
     } catch (err) {
@@ -43,15 +46,17 @@ export class StaffsController {
   }
 
   @Get()
+  @ApiOperation({ summary: 'Retrieve all staff members' })
   @ApiResponse({
     status: 200,
     description: 'Retrieves all staff members',
   })
-  async findAll(): Promise<Staff[]> {
+  async findAll() {
     return this.staffsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Retrieve a staff member by ID' })
   @ApiResponse({
     status: 200,
     description: 'Retrieves a specific staff member by ID',
@@ -60,7 +65,7 @@ export class StaffsController {
     status: 404,
     description: 'Staff member not found',
   })
-  async findOne(@Param('id') id: string): Promise<Staff> {
+  async findOne(@Param('id') id: string) {
     const staff = await this.staffsService.findOne(+id);
     if (!staff) {
       throw new HttpException(
@@ -72,15 +77,20 @@ export class StaffsController {
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a staff member by ID' })
   @ApiResponse({
     status: 200,
     description: 'Updates a specific staff member',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Failed to update staff member',
   })
   @ApiBody({ type: UpdateStaffDto })
   async update(
     @Param('id') id: string,
     @Body() updateStaffDto: UpdateStaffDto,
-  ): Promise<Staff> {
+  ) {
     try {
       return await this.staffsService.update(+id, updateStaffDto);
     } catch (err) {
@@ -92,6 +102,7 @@ export class StaffsController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a staff member by ID' })
   @ApiResponse({
     status: 200,
     description: 'Deletes a specific staff member',
