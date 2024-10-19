b556ef5ec199eb95e5410049aa0d9f8215c6fdd7
todsapol.nd@gmail.com <todsapol.nd@gmail.com>
2024-10-19 13:27:28 +0700

    refactor: :recycle: remove unused import

--- src/variant-options/dto/update-variant-option.dto.ts
+++ src/variant-options/dto/update-variant-option.dto.ts
@@ -1,5 +1,4 @@
 import { PartialType } from "@nestjs/swagger";
 import { CreateVariantOptionDto } from "./create-variant-option.dto";
-import { IsOptional } from "class-validator";
 
 export class UpdateVariantOptionDto extends PartialType(CreateVariantOptionDto) { }
